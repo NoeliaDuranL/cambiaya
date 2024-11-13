37d891f678823c42ad68a80e200208b24c25ff54
Noelia Paola Duran Llaveta <duran.llaveta.noeliapaola@usfx.bo>
2024-11-12 22:44:14 -0400

    añadido image controller y posproducto controller

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\NotificacionController;
 use App\Http\Controllers\MensajeController;
+use App\Http\Controllers\PostProductoController;
+use App\Http\Controllers\ImageController;
 
 // Rutas para los controladores usando apiResource
 Route::apiResource('persona', PersonaController::class);
@@ -15,6 +17,10 @@ Route::apiResource('producto', ProductoController::class);
 Route::apiResource('post', PostController::class);
 Route::apiResource('notificacion', NotificacionController::class);
 Route::apiResource('mensaje', MensajeController::class);
+Route::apiResource('post-producto', PostProductoController::class)->only(['store', 'destroy']);;
+Route::post('/upload-image', [ImageController::class, 'store']);
+
+
 
 // Ruta de ejemplo proporcionada por Laravel
 Route::get('/user', function (Request $request) {
